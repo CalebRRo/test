bda27752aec51f7cdf32f6b405ab165a9959527e
CalebRRo <yohannagraciano@gmail.com>
2022-05-24 17:33:23 -0300

    agrego argv y agrego switch

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const calculadora = require("./moodules/calculadora")
 const{argv} = require("process")
 
-const operacion = argc[2]
+const operacion = argc[2];
+const n1= argc[2];
+const n2 = argc[2];
+let resultado = 0;
+
+switch (operacion) {
+    case "sumar":
+        sumar (n1,n2)
+        break;
+
+    default:
+        break;
+}
 
 console.log(operacion);
